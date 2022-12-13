add9d8a816731cf8d4afe0209948cde0c0608d81
soon0223 <ksoon0223@gmail.com>
2022-12-13 10:39:10 +0900

    수정

--- src/store/about.js
+++ src/store/about.js
@@ -3,6 +3,6 @@ export default {
     state: () => ({
         name: 'SOON', 
         github: 'https://github.com/soon0223',
-        image: '././static/favicon.ico'
+        image: './static/favicon.ico'
     })
 }
